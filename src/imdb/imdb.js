54a97f47dc97c748735cdeb22c4b45804d9f32be
Michael Orlov <MiklerGM@gmail.com>
2019-06-16 17:08:39 +0200

    Sorting by year for imdb suggestions

--- src/imdb/imdb.js
+++ src/imdb/imdb.js
@@ -5,6 +5,13 @@ const filmsFromSuggestion = (json) => {
   if (!json || json.v !== 1 || !Array.isArray(json.d)) return [];
   const arr = json.d
     .filter(f => (f.q === 'feature' || f.q === 'video'))
+    .sort((a, b) => {
+      const r = b.y - a.y;
+      if (Number.isNaN(r)) {
+        if (b.y === undefined) return 1;
+      }
+      return r;
+    })
     .map(f => ({ label: f.l, id: f.id, year: f.y }));
   return arr;
 }
