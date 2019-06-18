a72b4d0853183dd75c56a06f32641dd82fc59cd6
Michael Orlov <MiklerGM@gmail.com>
2019-06-18 13:21:31 +0200

    imdb link generation

--- src/imdb/imdb.js
+++ src/imdb/imdb.js
@@ -1,6 +1,3 @@
-// https://v2.sg.media-imdb.com/suggestion/b/avengers.json
-
-
 const filmsFromSuggestion = (json) => {
   if (!json || json.v !== 1 || !Array.isArray(json.d)) return [];
   const arr = json.d
@@ -16,4 +13,26 @@ const filmsFromSuggestion = (json) => {
   return arr;
 }
 
-module.exports = { filmsFromSuggestion };
+const escapeSearchString = (a) => {
+  const G = /[àÀáÁâÂãÃäÄåÅæÆçÇèÈéÉêÊëËìÍíÍîÎïÏðÐñÑòÒóÓôÔõÕöÖøØùÙúÚûÛüÜýÝÿþÞß]/;
+  if (a) {
+    let b = a.toLowerCase();
+    return b.length > 20 && (b = b.substr(0, 20)),
+    b = b.replace(/^\s*/, "").replace(/[ ]+/g, "_"),
+    G.test(b) && (
+      b = b.replace(/[àÀáÁâÂãÃäÄåÅæÆ]/g, "a").replace(/[çÇ]/g, "c")
+        .replace(/[èÈéÉêÊëË]/g, "e").replace(/[ìÍíÍîÎïÏ]/g, "i").replace(/[ðÐ]/g, "d")
+        .replace(/[ñÑ]/g, "n").replace(/[òÒóÓôÔõÕöÖøØ]/g, "o").replace(/[ùÙúÚûÛüÜ]/g, "u")
+        .replace(/[ýÝÿ]/g, "y").replace(/[þÞ]/g, "t").replace(/[ß]/g, "ss")),
+    b = b.replace(/[\W]/g, "");
+  }
+  return '';
+}
+
+const getLink = (s = '', u = '') => ([
+  'https://v2.sg.media-imdb.com',
+  `suggestion${u ? `/${u}` : ''}`,
+  s.substr(0, 1),
+  `${s}.json`]).join('/');
+
+module.exports = { filmsFromSuggestion, escapeSearchString, getLink };
--- src/imdb/imdb.test.js
+++ src/imdb/imdb.test.js
@@ -1,50 +1,36 @@
 const imdb = require('./imdb');
 
-describe('films from suggestion', () => {
-  const avengers = require('./suggestion/avengers.test.json');
-  const avengersRes = [
-    { label: 'Avengers: Endgame', id: 'tt4154796', year: 2019 },
-    { label: 'Avengers: Infinity War', id: 'tt4154756', year: 2018 },
-    { label: 'Avengers: Age of Ultron', id: 'tt2395427', year: 2015 },
-    { label: 'The Avengers', id: 'tt0848228', year: 2012 },
-    { label: 'The Avengers', id: 'tt0118661', year: 1998 }
-  ];
-
-  const batman = require('./suggestion/batman.test.json');
-  const batmanRes = [
-    { label: 'The Batman', id: 'tt1877830', year: 2021 },
-    { label: "Batman vs Teenage Mutant Ninja Turtles", id: "tt9775360", year: 2019 },
-    { label: 'Batman v Superman: Dawn of Justice', id: 'tt2975590', year: 2016 },
-    { label: 'Batman Begins', id: 'tt0372784', year: 2005 },
-    { label: 'Batman & Robin', id: 'tt0118688', year: 1997 },
-    { label: 'Batman Forever', id: 'tt0112462', year: 1995 },
-    { label: 'Batman Returns', id: 'tt0103776', year: 1992 },
-    { label: 'Batman', id: 'tt0096895', year: 1989 },
-  ];
-
-  const strange = require('./suggestion/doctor_strange.test.json');
-  const strangeRes = [
-    { label: 'Doctor Strange 2', id: 'tt9419884', year: undefined },
-    { label: 'Doctor Strange: Strange Company',
-      id: 'tt6599680',
-      year: 2017 },
-    { label: 'Doctor Strange: The Score-Cerer Supreme',
-      id: 'tt6599762',
-      year: 2017 },
-    { label: 'Doctor Strange', id: 'tt1211837', year: 2016 },
-    { label: 'Doctor Strange', id: 'tt0910865', year: 2007 },
-    {
-      label: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-      id: 'tt0057012',
-      year: 1964 },
-  ];
-
+describe('imdb suggestions', () => {
   test('Select films', () => {
+    const avengers = require('./suggestion/avengers.test.json');
+    const avengersRes = require('./suggestion/avengers.res.json');
+  
+    const batman = require('./suggestion/batman.test.json');
+    const batmanRes = require('./suggestion/batman.res.json');
+  
+    const strange = require('./suggestion/doctor_strange.test.json');
+    const strangeRes = require('./suggestion/doctor_strange.res.json');
+
     expect(imdb.filmsFromSuggestion()).toEqual([]);
     expect(imdb.filmsFromSuggestion({})).toEqual([]);
     expect(imdb.filmsFromSuggestion(avengers)).toEqual(avengersRes);
     expect(imdb.filmsFromSuggestion(batman)).toEqual(batmanRes);
     expect(imdb.filmsFromSuggestion(strange)).toEqual(strangeRes);
+  });
+
+  test('Escape Search Phrase', () => {
+    expect(imdb.escapeSearchString()).toEqual('');
+    expect(imdb.escapeSearchString('The Batman')).toEqual('the_batman');
+    expect(imdb.escapeSearchString('Let\'s Dance')).toEqual('lets_dance');
+    expect(imdb.escapeSearchString('Mädchen in Uniform')).toEqual('madchen_in_uniform');
+    expect(imdb.escapeSearchString('Das weiße Band')).toEqual('das_weisse_band');
+  });
+
+  test('Generate link', () => {
+    expect(imdb.getLink('vampyr'))
+      .toEqual('https://v2.sg.media-imdb.com/suggestion/v/vampyr.json');
+    expect(imdb.getLink('batman', 'title'))
+      .toEqual('https://v2.sg.media-imdb.com/suggestion/title/b/batman.json');
   });
 })
 
