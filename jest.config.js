f9520aeefb9a2c37f2d409152afd62dadf03bf75
Michael Orlov <MiklerGM@gmail.com>
2020-04-05 13:07:47 +0200

    jest and ts configs

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfigFile: 'tsconfig.json'
+    }
+  },
+  moduleFileExtensions: [
+    'ts',
+    'js'
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,js,jsx}'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': './node_modules/ts-jest/preprocessor.js'
+  },
+  testMatch: [
+    '**/test/**/*.test.(ts|js)'
+  ],
+  testEnvironment: 'node'
+};
